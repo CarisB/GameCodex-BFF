76731e514efc2d9c59c12c0c3dd83aa107e70b0a
CarisB <chkbaek@gmail.com>
2024-06-06 11:36:35 -0500

    Added single game route

--- api/app.py
+++ api/app.py
@@ -20,3 +20,11 @@ def get_all(endpoint):
   params.update(request.args) # The URL query args
   res = requests.get(baseURL + request.path, headers=headers, params=params)
   return res.json()
+
+# Route for retrieving singular game
+@app.get('/game/<id>')
+def get(id):
+  endpoint = '/games'
+  url = baseURL + endpoint + '/' + id
+  res = requests.get(url, headers=headers, params=params)
+  return res.json()
