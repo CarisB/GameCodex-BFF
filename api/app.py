68c27908ede17ebc3cc3fca656f6711dbba1f621
CarisB <chkbaek@gmail.com>
2024-06-06 10:52:53 -0500

    Added Games route

--- api/app.py
+++ api/app.py
@@ -1,21 +1,25 @@
 from flask import Flask
+from flask import request
 from dotenv import load_dotenv
 import os
 import requests
 from requests.auth import HTTPBasicAuth
-import json
 
 app = Flask(__name__)
 
 load_dotenv()
-baseURL = os.environ.get('API_URL')
-apiKey = os.environ.get('API_KEY')
-endpoint = "/games"
+baseURL: str = os.environ.get('API_URL')
+apiKey: str = os.environ.get('API_KEY')
+
 headers = {'Accept': 'application/json'}
-payload = {'key': apiKey}
 
-res = requests.get(baseURL + endpoint, headers=headers, params=payload)
+# Games route
+@app.get('/games')
+def get_games():
+  endpoint = '/games'
+  params = {'key': apiKey}
+  params.update(request.args) # The URL query args
 
-@app.get('/')
-def testing():
+  res = requests.get(baseURL + endpoint, headers=headers, params=params)
+
   return res.json()
