257508e81198986c966f1ecb1295348375936077
CarisB <chkbaek@gmail.com>
2024-06-06 09:18:20 -0500

    Added initial API consumption

--- api/app.py
+++ api/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 app = Flask(__name__)
 
+load_dotenv()
+baseURL = os.environ.get('API_URL')
+apiKey = os.environ.get('API_KEY')
+endpoint = "/games"
+headers = {'Accept': 'application/json'}
+payload = {'key': apiKey}
+
+res = requests.get(baseURL + endpoint, headers=headers, params=payload)
+
 @app.get('/')
 def testing():
-  return "TESTING."
+  return res.json()
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8000, debug=True)
