727b73751d6a319ea78ade0cd84f116317018d05
CarisB <chkbaek@gmail.com>
2024-06-06 09:26:34 -0500

    Removed debug run code

--- api/app.py
+++ api/app.py
@@ -19,6 +19,3 @@ res = requests.get(baseURL + endpoint, headers=headers, params=payload)
 @app.get('/')
 def testing():
   return res.json()
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8000, debug=True)
